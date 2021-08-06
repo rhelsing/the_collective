0e2d5b71ed98e2e4400014171847206c2c0c7a85
Ryan Helsing <ryanhelsing@gmail.com>
2021-08-06 10:38:17 -0400

    annotated models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  user_id    :bigint           not null
+#  tagline    :string
+#  is_private :boolean          default(FALSE)
+#  slug       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Project < ApplicationRecord
   belongs_to :user
 end
